2c8435d9def2f3e1beddf85ccaf480b7487a2412
jcamilo78 <62293675+jcamilo78@users.noreply.github.com>
2022-03-30 23:44:23 +0200

    RoutingWebPage

--- complete-node-bootcamp-master/WorkArea_jcamilo/Part_1/index.js
+++ complete-node-bootcamp-master/WorkArea_jcamilo/Part_1/index.js
@@ -1,5 +1,8 @@
 const fs  = require('fs');
 const http = require('http');
+const url = require('url');
+
+/////////////////////////////////////////////
 // FILES
 
 // Blocking, synchronous way
@@ -38,13 +41,27 @@ console.log('File written!!-)')
 const testFile = fs.readFileSync('./txt/start.txt', 'utf-8');
 console.log(testFile);
 */
-// Server
+/////////////////////////////////
+// SERVER
 
 const server = http.createServer((req, res) => {
-    // console.log(req);
-    res.end('Hello from the the server!');
+    // console.log(req.url);
+    const pathName = req.url
+    if(pathName === '/' || pathName === '/overview'){
+        res.end('This is the OVERVIEW!!!-)!');
+    }else if (pathName === '/product'){
+        res.end('This is the PRODUCT!-)!');
+    } else {
+        res.writeHead(404, {
+            'Content_type': 'html',
+            'my-own-headr':'Hello World!!!'
+        });
+        res.end('Page not found!');  
+    }
 });
 // 127.0.0.1 this is the standard ip for localhost
 server.listen(8000, '127.0.0.1', () => {
     console.log('Listening to requiests on port 8000')
 });
+
+//ROUTING THE WEB
