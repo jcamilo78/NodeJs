c94dd8230f4b19dedb999db5d3509d0cd2d34e1d
jcamilo78 <62293675+jcamilo78@users.noreply.github.com>
2022-04-24 00:17:35 +0200

    Create my own module and learning NPM intro

--- complete-node-bootcamp-master/WorkArea_jcamilo/Part_1/index.js
+++ complete-node-bootcamp-master/WorkArea_jcamilo/Part_1/index.js
@@ -1,8 +1,10 @@
+// First call the global module 
 const fs  = require('fs');
 const http = require('http');
 const { json } = require('stream/consumers');
 const url = require('url');
-
+//Second call local project modules
+const replaceTemplate = require('./modules/replaceTemplate');
 /////////////////////////////////////////////
 // FILES
 
@@ -48,20 +50,20 @@ console.log(testFile);
 // in this way is more efficient because we just load once
 // data from templates folder to load once when the apps in init in this way is more efficient
 // in this way e create a function to dymamicilly changes the selection and the /g mean that is a for global use this variables
-const replaceTemplate = (temp, product) => {
-    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-    output = output.replace(/{%IMAGE%}/g, product.image);
-    output = output.replace(/{%PRICE%}/g, product.price);
-    output = output.replace(/{%FROM%}/g, product.from);
-    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-    output = output.replace(/{%QUANTITY%}/g, product.quantity);
-    output = output.replace(/{%DESCRIPTION%}/g, product.description);
-    output = output.replace(/{%ID%}/g, product.id);
-    
-    if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
-    return output;
-}
-
+// const replaceTemplate = (temp, product) => {
+    // let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+    // output = output.replace(/{%IMAGE%}/g, product.image);
+    // output = output.replace(/{%PRICE%}/g, product.price);
+    // output = output.replace(/{%FROM%}/g, product.from);
+    // output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+    // output = output.replace(/{%QUANTITY%}/g, product.quantity);
+    // output = output.replace(/{%DESCRIPTION%}/g, product.description);
+    // output = output.replace(/{%ID%}/g, product.id);
+    // 
+    // if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+    // return output;
+// }
+// 
 const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`,'utf-8');
 const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`,'utf-8');
 const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`,'utf-8');
